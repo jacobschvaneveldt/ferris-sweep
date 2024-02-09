166e796e4344ecd8391a405ac6e47e38782ba447
jacobschvaneveldt <jacobschvaneveldt@gmail.com>
2024-02-09 06:42:05 -0700

    removed unnecessary code

--- keymap.c
+++ keymap.c
@@ -19,10 +19,6 @@ enum keycodes {
     OS_CMD
 };
 
-enum ferris_tap_dances {
-  TD_Q_ESC
-};
-
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -43,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_QWERTY] = LAYOUT(    
-    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, 
-                             OS_SHFT, LOWER,       RAISE,   KC_SPC
+    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, 
+                             OS_SHFT, LOWER,         RAISE,   KC_SPC
   ),
 
 
